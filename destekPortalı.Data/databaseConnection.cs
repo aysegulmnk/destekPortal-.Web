92e1363a876d3fe5399b01ae089c97e76f709e0d
aysegulmnk <aysegul.mnik1999@gmail.com>
2023-09-12 22:07:32 +0300

    commit2

---
+++ destekPortalı.Data/databaseConnection.cs
@@ -0,0 +1,36 @@
+﻿using Oracle.ManagedDataAccess.Client; // Oracle veritabanı bağlantısı için gerekli kütüphane
+using System.Data;
+
+class Program
+{
+    static void Main()
+    {
+        // Oracle veritabanı bağlantı dizesini oluşturun
+        string connectionString = "User Id=C##_aysegul;Password=minik;Data Source=localhost";
+
+        // Bağlantıyı oluşturun
+        OracleConnection connection = new OracleConnection(connectionString);
+
+        try
+        {
+            // Bağlantıyı açın
+            connection.Open();
+
+            // Burada veritabanı işlemlerini gerçekleştirebilirsiniz
+
+            Console.WriteLine("Bağlantı başarılı!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Bağlantı hatası: " + ex.Message);
+        }
+        finally
+        {
+            // Bağlantıyı kapatın
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
